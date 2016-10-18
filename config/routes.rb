6b98d1cd88151df43d3199b757b4bbd3b0680df2
Trish Griego <pmgriego@gmail.com>
2016-10-18 15:50:15 -0700

    Created products controller, sessions controller, user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,32 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
+  get 'products/index'
+
+  get 'products/show'
+
+  get 'products/new'
+
+  get 'products/create'
+
+  get 'products/edit'
+
+  get 'products/update'
+
+  get 'products/destroy'
+
   get 'sessions/index'
 
   get 'sessions/show'
