eb09d061bd785f8a893eac24dca57f3c395b34b5
Maya Wang <salome.neptune@gmail.com>
2016-10-18 17:31:24 -0700

    completed model relationships

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ActiveRecord::Base
   belongs_to :user_id
+  has_many :order_id
+  has_many :order_item
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :review_id
+  has_many :product_id
+  has_many :user_id
 end
