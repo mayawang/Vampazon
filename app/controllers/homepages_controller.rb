e639eb9c49606837b759940ad64b9e7ae421081a
Maya Wang <salome.neptune@gmail.com>
2016-10-19 22:03:00 -0700

    fix bugs on previous built features

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,12 +1,12 @@
 class HomepagesController < ApplicationController
 
-  def root
+  def index
     @product_categories = Product.uniq.pluck(:category)
   end
 
   def show_category
     @product_category = params[:category]
-    @products_in_category = Product.where(:category)
+    @products_in_category = Product.where(:category => @product_category)
   end
 
 end
