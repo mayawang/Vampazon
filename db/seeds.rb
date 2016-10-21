1b791acabdc3d26fa33d5e0ac4625239804f9898
Maya Wang <salome.neptune@gmail.com>
2016-10-20 22:28:23 -0700

    backup prepare for create user data base

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,31 @@ require 'csv'
   CSV.foreach ('seed_csvs/products.csv') do |csv_obj|
     Product.create(name: csv_obj[1], description: csv_obj[2], price: csv_obj[3], photo_url: csv_obj[4], inventory: csv_obj[5], category: csv_obj[6])
   end
+
+# 
+# require 'faker'
+#
+# 10.times do
+#    User.create(
+#       first_name: Faker::Name.first_name,
+#       last_name:
+#       email: Faker::Internet.email,
+#       street_address: Faker::Hipster.paragraph,
+#       city: "Book",
+#       state:
+#       zip: Faker::Address.zip
+#       phone:
+#     )
+# end
+#
+# t.string   "first_name"
+# t.string   "last_name"
+# t.string   "email"
+# t.string   "street_address"
+# t.string   "city"
+# t.string   "state"
+# t.integer  "zip"
+# t.integer  "phone"
+# t.boolean  "merchant"
+# t.datetime "created_at",     null: false
+# t.datetime "updated_at",     null: false
