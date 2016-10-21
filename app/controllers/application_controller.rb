f715ebcb0330aa319d3021b7c6c534f14c8da25f
RedSquirrelious <redsquirrelious@gmail.com>
2016-10-21 10:12:58 -0700

    commented out scratch authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,12 @@ class ApplicationController < ActionController::Base
 private
 
 	 def current_user
-	 	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+	 	#AUTHENTICATION FROM SCRATCH METHOD START
+	 	# @current_user ||= User.find(session[:user_id]) if session[:user_id]
+	 	#AUTHENTICATION FROM SCRATCH METHOD END
+
+		@user = User.find_by(id: session[:user_id] )
+
+	 	# @current_user = User.find_by(id: auth_hash[:id], provider: 'github') if session[:user_id]
 	 end
 end
