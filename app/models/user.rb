687a305ae274714f123743af2a11785a20e8fb42
RedSquirrelious <redsquirrelious@gmail.com>
2016-10-21 12:19:17 -0700

    commented out has_secure_password, uncommented validators and build by github

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,19 @@
 class User < ActiveRecord::Base
   has_many :reviews
   has_many :products
-  has_secure_password
+  # has_secure_password
 
 
-  # validates :email, :id, :provider, presence: true
+  validates :email, :uid, :provider, presence: true
 
-  # def self.build_from_github(auth_hash)
-  #   user       = User.new
-  #   user.id   = auth_hash[:id]
-  #   user.provider = 'github'
-  #   user.first_name  = auth_hash['info']['first_name']
-  #   user.last_name  = auth_hash['info']['last_name']
-  #   user.email = auth_hash['info']['email']
+  def self.build_from_github(auth_hash)
+    user       = User.new
+    user.uid   = auth_hash[:uid]
+    user.provider = 'github'
+    user.first_name  = auth_hash['info']['first_name']
+    user.last_name  = auth_hash['info']['last_name']
+    user.email = auth_hash['info']['email']
 
-  #   return user
-  # end
+    return user
+  end
 end
