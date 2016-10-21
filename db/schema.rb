4bf5263831292c08043beff148bd746226a3720c
RedSquirrelious <redsquirrelious@gmail.com>
2016-10-20 19:29:13 -0700

    fixed schema numbering, not sure why it didn't take when i did the merge...

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20161020033512) do
-=======
+
 ActiveRecord::Schema.define(version: 20161021021144) do
->>>>>>> master
+
 
   create_table "order_items", force: :cascade do |t|
     t.decimal  "unit_price"
