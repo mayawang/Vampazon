eb978b3b82e8e11c9690bc82f7f2fa557737da78
RedSquirrelious <redsquirrelious@gmail.com>
2016-10-20 23:31:00 -0700

    added has_secure_password validation, commented out github authentication method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,19 @@
 class User < ActiveRecord::Base
   has_many :reviews
   has_many :products
+  has_secure_password
 
 
-  validates :email, :id, :provider, presence: true
+  # validates :email, :id, :provider, presence: true
 
-  def self.build_from_github(auth_hash)
-    user       = User.new
-    user.id   = auth_hash[:id]
-    user.provider = 'github'
-    user.first_name  = auth_hash['info']['first_name']
-    user.last_name  = auth_hash['info']['last_name']
-    user.email = auth_hash['info']['email']
+  # def self.build_from_github(auth_hash)
+  #   user       = User.new
+  #   user.id   = auth_hash[:id]
+  #   user.provider = 'github'
+  #   user.first_name  = auth_hash['info']['first_name']
+  #   user.last_name  = auth_hash['info']['last_name']
+  #   user.email = auth_hash['info']['email']
 
-    return user
-  end
+  #   return user
+  # end
 end
