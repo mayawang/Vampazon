8a90d51292c7c07be23c19299cba582c5977aef4
RedSquirrelious <redsquirrelious@gmail.com>
2016-10-21 12:23:01 -0700

    added true test, added options for other controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  test 'should always succeed' do
+    assert true
+  end
+
   test "should get index" do
     get :index
     assert_response :success
@@ -16,10 +20,10 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  # test "should get create" do
-  #   get :create
-  #   assert_response :success
-  # end
+  test "should get create" do
+    get :create
+    assert_response :success
+  end
 
   test "should get edit" do
     get :edit, { id: users(:rbg).id }
@@ -32,7 +36,35 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should be able to deactivate a user (i.e., destroy)" do
-
+    get :destroy
+    assert_response :success
   end
+
+#FOR ALL OTHER CONTROLLERS
+  # test "If a user is not logged in they cannot see their task." do
+  #   session[:user_id] = nil  # ensure no one is logged in
+
+  #   get :show, id: tasks(:grace_task).id
+  #   # if they are not logged in they cannot see the resource and are redirected to login.  
+  #   assert_redirected session_path
+  #   assert_equal "You must be logged in first", flash[:notice]
+  # end
+
+  # test "Make sure a user can see their tasks" do
+  #   session[:user_id] = users(:grace_hopper).id
+  #   get :show, id: tasks(:grace_task).id
+    
+  #   assert_response :success
+  # end
+
+  # test "Make Sure a User Cannot See Another User's Tasks" do
+  #   session[:user_id] = users(:weili_dai).id
+  #   get :show, id: tasks(:grace_task).id
+  #   get :show, id: tasks(:grace_task).id
+    
+  #   assert_response :redirect
+  #   assert_equal flash[:notice], "You do not have access to that task."
+  # end
+
 
 end
