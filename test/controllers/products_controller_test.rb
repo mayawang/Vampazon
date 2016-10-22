3ae5b2f4584d7dded0f8871a5c43b84672051fb4
Trish Griego <pmgriego@gmail.com>
2016-10-21 18:38:03 -0700

    Created minimum testing.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -7,61 +7,86 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should get show" do
-    get :show
+    get :show, id: products(:product1).id
     assert_response :success
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
 
-  test "should get create" do
-    get :create
-    assert_response :success
-  end
+  test 'Non-authenticated user should NOT be able to create a new product' do
+    session[:user_id] = nil
 
-  test "should get edit" do
-    get :edit
-    assert_response :success
-  end
+    post :create, {product: {name: "RBG", description: "Yada yada", price: "2.00", inventory: 2, category: "Houseware", user_id: nil}}
 
-  test "should get update" do
-    get :update
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get :destroy
-    assert_response :success
-  end
-
-  #example used was in TaskRails Task Controller, needs fleshing out with fixture info
-
-    test "If a user is not logged in they cannot see their product." do
-    session[:user_id] = nil  # ensure no one is logged in
-
-    get :show, id: products(:SOMEPRODUCT).id
-    # if they are not logged in they cannot see the resource and are redirected to login.  
-    assert_redirected session_path
     assert_equal "You must be logged in first", flash[:notice]
-  end
-
-  test "Make sure a user can see their products" do
-    session[:user_id] = users(:SOMENAME).id
-    get :show, id: products(:SOMEPRODUCT).id
-    
-    assert_response :success
-  end
-
-  test "Make Sure a User Cannot See Another User's products" do
-    session[:user_id] = users(:SOMENAME).id
-    get :show, id: products(:SOMEPRODUCT).id
-    get :show, id: products(:SOMEPRODUCT).id
-    
     assert_response :redirect
-    assert_equal flash[:notice], "You do not have access to that information."
   end
 
+  # test "If a user is not logged in they cannot see their task." do
+  #   session[:user_id] = nil  # ensure no one is logged in
+
+  #   get :show, id: tasks(:grace_task).id
+  #   # if they are not logged in they cannot see the resource and are redirected to login.
+  #   assert_redirected session_path
+  #   assert_equal "You must be logged in first", flash[:notice]
+  # end
+
+
+  #   get :show, id: tasks(:grace_task).id
+  #   # if they are not logged in they cannot see the resource and are redirected to login.
+  #   assert_redirected session_path
+  #   assert_equal "You must be logged in first", flash[:notice]
+  # end
+  # test "should get new" do
+  #   get :new
+  #   assert_response :success
+  # end
+  #
+  # test "should get create" do
+  #   get :create
+  #   assert_response :success
+  # end
+  #
+  # test "should get edit" do
+  #   get :edit
+  #   assert_response :success
+  # end
+  #
+  # test "should get update" do
+  #   get :update
+  #   assert_response :success
+  # end
+  #
+  # test "should get destroy" do
+  #   get :destroy
+  #   assert_response :success
+  # end
+  #
+  # #example used was in TaskRails Task Controller, needs fleshing out with fixture info
+  #
+  #   test "If a user is not logged in they cannot see their product." do
+  #   session[:user_id] = nil  # ensure no one is logged in
+  #
+  #   get :show, id: products(:SOMEPRODUCT).id
+  #   # if they are not logged in they cannot see the resource and are redirected to login.
+  #   assert_redirected session_path
+  #   assert_equal "You must be logged in first", flash[:notice]
+  # end
+  #
+  # test "Make sure a user can see their products" do
+  #   session[:user_id] = users(:SOMENAME).id
+  #   get :show, id: products(:SOMEPRODUCT).id
+  #
+  #   assert_response :success
+  # end
+  #
+  # test "Make Sure a User Cannot See Another User's products" do
+  #   session[:user_id] = users(:SOMENAME).id
+  #   get :show, id: products(:SOMEPRODUCT).id
+  #   get :show, id: products(:SOMEPRODUCT).id
+  #
+  #   assert_response :redirect
+  #   assert_equal flash[:notice], "You do not have access to that information."
+  # end
+  #
 
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -28,34 +28,34 @@ class SessionsControllerTest < ActionController::TestCase
   #   assert_response :success
   # end
 
-  test "should get destroy" do
-    get :destroy
-    assert_response :redirect
-  end
-
-  def login_a_user
-    request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:github]
-    get :create,  {provider: "github"}
-  end
-
-  test "Can Create a user" do
-    assert_difference("User.count", 1) do
-      login_a_user
-      assert_response :redirect
-      assert_redirected_to root_path
-    end
-  end
-
-  test "If a user logs in twice it doesn't create a 2nd user" do
-    assert_difference("User.count", 1) do
-      login_a_user
-    end
-    assert_no_difference("User.count") do
-      login_a_user
-      assert_response :redirect
-      assert_redirected_to root_path
-    end
-  end
+  # test "should get destroy" do
+  #   get :destroy
+  #   assert_response :redirect
+  # end
+  #
+  # def login_a_user
+  #   request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:github]
+  #   get :create,  {provider: "github"}
+  # end
+  #
+  # test "Can Create a user" do
+  #   assert_difference("User.count", 1) do
+  #     login_a_user
+  #     assert_response :redirect
+  #     assert_redirected_to root_path
+  #   end
+  # end
+  #
+  # test "If a user logs in twice it doesn't create a 2nd user" do
+  #   assert_difference("User.count", 1) do
+  #     login_a_user
+  #   end
+  #   assert_no_difference("User.count") do
+  #     login_a_user
+  #     assert_response :redirect
+  #     assert_redirected_to root_path
+  #   end
+  # end
 
   #   test "should get create" do
   #   get :create
