334ed0a1c7fd5ac0cd69da060eaaae0c89e2d386
Trish Griego <pmgriego@gmail.com>
2016-10-22 19:23:58 -0700

    saving changes

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -16,58 +16,16 @@ class OrderTest < ActiveSupport::TestCase
 
 # You have access to all fixtures via a method with the same name  orders(:order1),(:order2)
 
-# this will not pass without proper validations. Which seem necesssary if orders must have certain info?
-  # test "Order without proper information is not valid" do
-  #   orders(:minimal_order).valid?
-  #   assert_not orders(:minimal_order).valid?
-  # end
-
-
-  # test "add to cart should work" do
-  #
-  # end
-
-  # test "An order should belong to product" do
-  #   assert orders(:order1)
-  # end
   test "Order 1 must have first_name of Ruth" do
       assert_equal "Ruth",
        orders(:one).first_name
   end
 
-  # test "Can create a new order" do
-  #
-  # end
-
-  # test "add and remove product from order" do
-  #
-  #   sunscreen = orders(:product1)
-  #   coffin = orders(:product2)
-  #
-  #   sunscreen.add_to_cart()
-  #
-  #   coffin.remove_cart()
-  #
-  #
-  # end
-
-  # test "You can add proudct to order" do
-  #   assert_difference(Order.count, -1)
-  #   # controller here would delete something
-  #
-  # end
-
-  # test "Orders can have products" do
-  #   assert_includes products(:product1).orders, products(:sunscreen)
-  # end
 
   test "order one's id equals 1" do
     assert orders(:one).id == 1
   end
 
-  # test "order one's order items include product1" do
-  #
-  # end
 
   test "does not save order without status" do
     order_one = Order.new
@@ -79,7 +37,9 @@ class OrderTest < ActiveSupport::TestCase
     assert_equal things.user_id, 1
   end
 
-
+  test "Order with minimal information is valid" do
+    
+  end
 
 
 
