2318b6937dd1eec4ec1213cdc8d79392edaf347d
Elle Vargas <vargas.elle@gmail.com>
2016-10-24 14:07:48 -0700

    updated user ymls to use only one name and corrected call error in user rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,13 @@ class User < ActiveRecord::Base
   has_many :products
   # has_secure_password
 
-
   validates :email, :uid, :provider, presence: true
 
   def self.build_from_github(auth_hash)
     user       = User.new
     user.uid   = auth_hash[:uid]
     user.provider = 'github'
-    user.name  = auth_hash['raw']['name']
+    user.name  = auth_hash['info']['name']
     user.email = auth_hash['info']['email']
     #raise to get info for auth hash
     return user
