3a7ac4097956797f7b18e070b6fcdcaf7e60eb05
Maya Wang <salome.neptune@gmail.com>
2016-10-23 18:19:01 -0700

    add subtotal order price in cart, fixed duplicated order_items show in cart bug

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -17,19 +17,39 @@ class OrdersController < ApplicationController
     end
 
     @product = Product.find(params[:product_id])
-    @order_item = OrderItem.new
-    @order_item.order_id = session[:cart_id]
-    @order_item.quantity = params[:quantity].to_i
-    @order_item.unit_price = @product.price
-    @order_item.product_id = @product.id
-    @order_item.save!
+    # if user puts more than one order_items with same product_id in cart in seperate occasions, there should be only one order_item for this product in cart.
+    # Just update the total quantity of the order_items.
 
+    # [TEST]write a test case here!
+
+    order_items = OrderItem.where(:order_id => @order.id, :product_id => params[:product_id])
+
+    if order_items.length >= 1
+      @order_item = order_items.first
+      @order_item.quantity += params[:quantity].to_i
+      @order_item.save!
+    else
+
+
+      @order_item = OrderItem.new
+      @order_item.order_id = session[:cart_id]
+      @order_item.quantity = params[:quantity].to_i
+      @order_item.unit_price = @product.price
+      @order_item.product_id = @product.id
+      @order_item.save!
+    end
     redirect_to action: 'show_cart'
 
   end
 
   def show_cart
-    @order_items = OrderItem.where(:order_id => session[:cart_id])
+    if session[:cart_id]
+      @order_items = OrderItem.where(:order_id => session[:cart_id])
+    else
+      @order_items = []
+    end
+
+    @order = Order.find_by(:id => session[:cart_id]) || Order.new
   end
 
   def checkout
