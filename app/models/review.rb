87377e4b4fdd37c2824cb39daadda2b6ed9fffd1
RedSquirrelious <redsquirrelious@gmail.com>
2016-10-28 09:41:54 -0700

    related to user through product, commented out number validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,9 @@
 class Review < ActiveRecord::Base
-  belongs_to :user
-  # validates :rank, presence: true, only_integer: true, :inclusion => 1..5, message: "only allows integers 1-5"
+  belongs_to :product
+  belongs_to :user, through: :product 
+  validates :rank, presence: true
+  # , only_integer: true, :inclusion => 1..5 
+  #, message: "only allows integers 1-5"
+
 
 end
