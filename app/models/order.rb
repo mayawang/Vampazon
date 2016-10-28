a31eea67b96e6969721cdb3d1d6af3d5d4e5fbb0
Maya Wang <salome.neptune@gmail.com>
2016-10-28 12:47:20 -0700

    bug fixed deleted validations on order model and order_item model

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
   has_many :order_items
-  validates :price, presence: true, numericality: {:greater_than => 0}
+
 
 
   def total_price
